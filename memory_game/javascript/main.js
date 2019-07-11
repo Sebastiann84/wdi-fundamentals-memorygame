f40d9e2d452f23d933a58692d4c06210dc191631
Sheena Sebastian <annsheenasebastian@gmail.com>
2019-07-11 12:32:09 -0700

    created game logic

---
+++ memory_game/javascript/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running");
+// Creating 4 varialbles for the 4 cards
+var cardOne = "Queen of Hearts";
+var cardTwo = "Queen of Diamonds";
+var cardThree = "King of Hearts";
+var cardFour = "King of Diamonds";
+console.log("User  flipped " + cardOne + ".");
+console.log("User  flipped " + cardFour + ".");
