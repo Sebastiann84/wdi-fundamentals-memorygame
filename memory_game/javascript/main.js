23331fe221787f730ca6789b8de43f8198b4aa9d
Sheena Sebastian <annsheenasebastian@gmail.com>
2019-07-15 09:02:06 -0700

    created game logic

--- memory_game/javascript/main.js
+++ memory_game/javascript/main.js
@@ -1,30 +1,44 @@
-
-// Creating 4 varialbles for the 4 cards
-/* var cardOne = "Queen of Hearts";
-var cardTwo = "Queen of Diamonds";
-var cardThree = "King of Hearts";
-var cardFour = "King of Diamonds";
-console.log("User  flipped " + cardOne + ".");
-console.log("User  flipped " + cardFour + ".");
-*/
-
-//alert('Hello, friends.');
-// this array holds the four cards
-var cards = ["Queen","Queen","King","King"];
+// this object holds the properties an values for four cards
+var cards = [
+  {
+    rank: "Queen",
+    suit: "Hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "Queen",
+    suit: "Diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "King",
+    suit: "Hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "King",
+    suit: "Diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 // Creating an empty array to store the cards that are in play
 var cardsInPlay = [];
+// Adding match logic wiht a conditional statement
 function checkForMatch(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You found a match!");
   } else {
-    console.log("Sorry, try again!");
+    alert ("Sorry, try again!");
   }
  };
 // creating a function to store all steps that should take place when user flips card
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
 };
+
 flipCard(0);
 flipCard(2);
 checkForMatch();
